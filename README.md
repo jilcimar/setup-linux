19663ee19173a8a44cae2426f8dda48431b05f09
jilcimar <jilcimar.fernandes0267@gmail.com>
2018-04-20 23:45:44 -0300

    Adding list of programs and desciption

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# setup-linux
+# Setting my PC
+
 Script to configure the usual programs and development of my PC.
+
+1. Download the "setuplinux.sh" file that is in the project
+2. On the terminal, grant the permissions to the file
+```
+chmod 777 setuplinux.sh
+```
+3. Then run the file
+```
+./setuplinux.sh
+```
+
+# List of programs contained in Script
+
+- Git
+- PHP 7.2
+- PostgreSQL
+- Laravel
+- Vim
+- Python 3.6
+- Pip3
+- Spotfy
---
+++ setuplinux.sh
@@ -0,0 +1,43 @@
+#Git
+sudo apt-get install git
+git config --global user.name "jilcimar"
+git config --global user.email "jilcimar.fernandes0267@gmail.com"
+
+#Instalação do Laravel + PHP
+sudo apt-add-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php-mbstring
+sudo apt-get install composer -y
+sudo apt-get install php7.2-pgsql -y
+sudo apt-get install apache2 libapache2-mod-php7.0 -y
+sudo apt-get install php7.2-dev -y
+sudo apt-get install php7.2-zip -y
+sudo apt-get install php7.2-xml -y
+sudo apt-get install php7.2-mbstring -y
+sudo apt-get install php7.2-cgi -y
+sudo apt-get install php7.2-curl -y
+sudo apt-get install php7.2-zip
+
+composer global require "laravel/installer"
+export PATH=$PATH:$HOME/.config/composer/vendor/bin
+
+#PostgreSQL
+sudo apt-get install postgresql pgadmin3
+
+#VIM
+sudo apt-get install vim
+
+#Setup Python3 + Pip3
+sudo apt install python3-pip
+sudo apt install python3-pip
+pip install --upgrade pip
+sudo apt update
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt update
+sudo apt-get install python3.6
+
+#SPOTFY
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
